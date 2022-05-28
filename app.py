fed03e1a81a0c18bdc3ee0c557fe604bc80a5592
sunil416 <sksunilkumar416@gmail.com>
2022-05-29 01:02:21 +0530

    added to logs to test the Logger

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask
+
+from App_Logger.AppLogger import AppLogger
+from Modules.Modules import ModuleName
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
+    logger=AppLogger()
+    logger.Log(ModuleName.General, "Started With home route")
     return "Hello, Welcome To Thyroid Prediction Project"
